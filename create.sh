aa2cf91b6c76c9c4d6af76e290f93a3f446e40b8
Jeff Haynie <jhaynie@shopmonkey.io>
2022-07-09 20:48:01 -0500

    attempt to get helm with kustomize working

--- create.sh
+++ create.sh
@@ -2,4 +2,5 @@
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch configmap/argocd-cm -n argocd --type merge -p '{"data":{"kustomize.buildOptions":"--enable-helm"}}'
 kubectl apply -k ./argo/overlays/dev
