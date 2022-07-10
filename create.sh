aa8ce6b2bd8fb2c378ad6078e7c2d2ebc8acf749
Jeff Haynie <jhaynie@shopmonkey.io>
2022-07-10 11:16:04 -0500

    add all components into platform namespace

--- create.sh
+++ create.sh
@@ -1,6 +1,23 @@
 #/usr/bin/bash
 
+# take the first argument as the environment to use
+env=${1:-dev}
+dir=./argo/overlays/$env
+
+if [ ! -d $dir ];
+then
+	echo "Couldn't find the directory $dir"
+	exit 1
+fi
+
+# create the argocd namespace
 kubectl create namespace argocd
+
+# install argo
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# patch argo to enable helm inside kustomize
 kubectl patch configmap/argocd-cm -n argocd --type merge -p '{"data":{"kustomize.buildOptions":"--enable-helm"}}'
-kubectl apply -k ./argo/overlays/dev
+
+# install the specific environment components
+kubectl apply -k $dir
